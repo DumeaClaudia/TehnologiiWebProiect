974f1054ff00dd3e2b0130ff42b0188456f08d53
Claudia <claudia.dumea18@gmail.com>
2017-06-11 17:24:31 +0300

    do not redirect if not loggedin in for game scores page

--- gamesScores.php
+++ gamesScores.php
@@ -2,16 +2,7 @@
  ob_start();
  session_start();
  require_once 'DBconnect.php';
- 
- // if session is not set this will redirect to login page
- if( !isset($_SESSION['user']) ) {
-  header("Location: loginHome.php");
-  exit;
- }
- // select loggedin users detail
- $res=mysql_query("SELECT * FROM users WHERE userId=".$_SESSION['user']);
- $userRow=mysql_fetch_array($res);
-?>
+ ?>
 <!DOCTYPE html>
 <html>
 
